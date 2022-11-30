806b205c7fe42d0eb5d88005eab14c956cdde303
BSMilev18 <BSMilev18@codingburgas.bg>
2022-11-30 15:24:39 +0200

    Initial commit

---
+++ src/main/java/io/datajek/springmvc/Athlete.java
@@ -0,0 +1,24 @@
+package io.datajek.springmvc;
+
+public class Athlete {
+    private String lastName;
+    private String country;
+
+    public Athlete(){}
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
